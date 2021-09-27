653743657277272d8bea143f728c947e1997b8de
ThoughtContagion <clittrell@soteria.io>
2021-09-27 10:25:18 -0400

    Update

--- Inspectors/MailboxesWithForwarding.ps1
+++ Inspectors/MailboxesWithForwarding.ps1
@@ -2,13 +2,12 @@
 
 Function Get-MailboxForwarding{
     $mailboxes = Get-EXOMailbox -Filter {(DeliverToMailboxAndForward -ne $false) -or (ForwardingAddress -ne $null) -or (ForwardingSmtpAddress -ne $null)} -ResultSize Unlimited
-    for ($i = 1; $i -le $mailboxes.count){
+    $i = 0
         foreach ($mailbox in $mailboxes){
             $mailbox | Select-Object displayname, windowsemailaddress, delivertomailboxandforward, forwardingaddress, forwardingsmtpaddress | Export-Csv "$(Get-Date -Format yyyy-MM-dd)_Mailboxes_with_forwarding.csv" -NoTypeInformation -Append
             $i++ 
-            Write-Progress -Activity "Searching Mailboxes" -Status "$i% Complete:" -PercentComplete ($i/$mailboxes.count*100)
+            Write-Progress -Activity "Searching Mailboxes" -PercentComplete ($i/$mailboxes.count*100)
         }
-    }
     Return $mailboxes.alias
 }
 
