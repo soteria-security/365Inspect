f05626b834295e3b086a86471cd73b1859416e74
ThoughtContagion <clittrell@soteria.io>
2021-09-30 12:31:31 -0400

    Updated MFA inspectors

--- Inspectors/UsersWithNoMFAConfigured.ps1
+++ Inspectors/UsersWithNoMFAConfigured.ps1
@@ -1,5 +1,5 @@
 function Inspect-UsersWithNoMFAConfigured {
-	$unenabled_users = (Get-MsolUser -All | Where-Object {($_.StrongAuthenticationMethods.Count -eq 0) -and ($_.BlockCredential -eq $False)}).UserPrincipalName
+	$unenabled_users = (Get-MsolUser -All | Where-Object {($_.StrongAuthenticationMethods.Count -eq 0) -and ($_.BlockCredential -eq $False) -and ($_.StrongAuthenticationRequirements.State -NE "Enforced")}).UserPrincipalName
 	
 	If ($unenabled_users -ne 0) {
 		return $unenabled_users
--- Inspectors/UsersWithNoMFAEnforced.ps1
+++ Inspectors/UsersWithNoMFAEnforced.ps1
@@ -1,7 +1,7 @@
 function Inspect-UsersWithNoMFAEnforced {
 	# -All has been broken for ~5 years, unfortunately.
 	# Might be a way to better query this with Graph or something.
-	$unenforced_users = (Get-MsolUserByStrongAuthentication -All | Where-Object {$_.StrongAuthenticationRequirements.State -NE "Enforced"}).UserPrincipalName
+	$unenforced_users = (Get-MsolUserByStrongAuthentication -MaxResults 999999 | Where-Object {$_.StrongAuthenticationRequirements.State -NE "Enforced"}).UserPrincipalName
 	$num_unenforced_users = $unenforced_users.Count
 
 	If ($num_unenforced_users -NE 0) {
