989916b1febac44d35f9e8e7bd278ae20c7eb98f
ThoughtContagion <clittrell@soteria.io>
2021-10-25 16:52:12 -0400

    Fixes to check for and exclude common false positives

--- Inspectors/MailboxAuditingEnabled.ps1
+++ Inspectors/MailboxAuditingEnabled.ps1
@@ -1,10 +1,15 @@
 function Inspect-MailboxAuditingEnabled {
-	$mailboxes_without_auditing = (Get-EXOMailbox -ResultSize Unlimited | Where-Object {!$_.AuditEnabled}).Name
-	If ($mailboxes_without_auditing.Count -NE 0) {
-		return $mailboxes_without_auditing
+	$audit_disabled = (Get-OrganizationConfig).AuditDisabled
+
+	If ($audit_disabled -eq $true){
+		$mailboxes_without_auditing = (Get-EXOMailbox -ResultSize Unlimited | Where-Object {!$_.AuditEnabled}).Name
+		If ($mailboxes_without_auditing.Count -NE 0) {
+			return $mailboxes_without_auditing
+			}
+		}
+	Else{
+		return $null
 	}
-	
-	return $null
 }
 
 return Inspect-MailboxAuditingEnabled
