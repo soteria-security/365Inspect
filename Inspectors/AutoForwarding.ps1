73defdf6796d962a7442cb2464b25e6b427a4db7
ThoughtContagion <clittrell@soteria.io>
2021-10-30 09:59:03 -0400

    Fixed false positives when external auto-forwarding is blocked via Outbound Spam Policy

--- Inspectors/AutoForwarding.ps1
+++ Inspectors/AutoForwarding.ps1
@@ -1,15 +1,19 @@
 function Inspect-AutoForwarding {
-	$rules = Get-TransportRule
-	$flag = $False
+	$externalForwarding = Get-HostedOutboundSpamFilterPolicy | Select-Object autoforwardingmode
 
-	ForEach ($rule in $rules) {
-		If (($rule.MessageTypeMatches -eq "AutoForward") -AND ($rule.DeleteMessage -OR $rule.RejectMessageReasonText)) {
-			$flag = $True
+    If ($externalForwarding -eq "On") {
+		$rules = Get-TransportRule
+		$flag = $False
+
+		ForEach ($rule in $rules) {
+			If (($rule.MessageTypeMatches -eq "AutoForward") -AND ($rule.DeleteMessage -OR $rule.RejectMessageReasonText)) {
+				$flag = $True
+			}
 		}
-	}
 
-	If (-NOT $flag) {
-		return @($org_name)
+		If (-NOT $flag) {
+			return @($org_name)
+		}
 	}
 	
 	return $null
