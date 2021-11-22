ee9fa7979d1f3d43f2c3b4d0e4d26030b58bb6be
ThoughtContagion <clittrell@soteria.io>
2021-11-22 16:19:45 -0500

    Bug fixes

--- Inspectors/BypassingSafeLinks.ps1
+++ Inspectors/BypassingSafeLinks.ps1
@@ -1,5 +1,5 @@
 function Inspect-BypassingSafeLinks {
-	$safe_links_bypass_rules = (Get-TransportRule | Where { $_.SetHeaderName -eq "X-MS-Exchange-Organization-SkipSafeLinksProcessing" }).Identity
+	$safe_links_bypass_rules = (Get-TransportRule | Where-Object {($_.State -eq "Enabled") -and ($_.SetHeaderName -eq "X-MS-Exchange-Organization-SkipSafeLinksProcessing")}).Identity
 	
 	If ($safe_links_bypass_rules.Count -ne 0) {
 		return $safe_links_bypass_rules
