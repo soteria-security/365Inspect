d8675819855cd842d8b4fd9511ba1a7d9ace9727
Evan Klein <evanm.klein@gmail.com>
2024-02-19 15:04:30 -0500

    Added UserEmailMFA Inspector
    Added a module to check for users with Email as a registered MFA method.

---
+++ Inspectors/Inspect-UserEmailMFA.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = "Stop"
+
+$errorHandling = "$((Get-Item $PSScriptRoot).Parent.FullName)\Write-ErrorLog.ps1"
+
+. $errorHandling
+
+
+function Inspect-UserEmailMFA {
+    Try {
+        return Get-MgReportAuthenticationMethodUserRegistrationDetail | Where-Object MethodsRegistered -CContains "email" | select UserPrincipalName, IsAdmin
+    }
+    Catch {
+        Write-Warning "Error message: $_"
+        $message = $_.ToString()
+        $exception = $_.Exception
+        $strace = $_.ScriptStackTrace
+        $failingline = $_.InvocationInfo.Line
+        $positionmsg = $_.InvocationInfo.PositionMessage
+        $pscommandpath = $_.InvocationInfo.PSCommandPath
+        $failinglinenumber = $_.InvocationInfo.ScriptLineNumber
+        $scriptname = $_.InvocationInfo.ScriptName
+        Write-Verbose "Write to log"
+        Write-ErrorLog -message $message -exception $exception -scriptname $scriptname
+        Write-Verbose "Errors written to log"
+    }
+}
+
+return Inspect-UserEmailMFA
